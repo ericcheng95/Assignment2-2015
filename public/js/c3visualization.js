b19b05cb823127aecab555598097f061554aae6c
Alvin Yan <apyan@ucsd.edu>
2015-04-27 16:51:51 -0700

    Small changes
    Some small changes

--- public/js/c3visualization.js
+++ public/js/c3visualization.js
@@ -2,16 +2,16 @@
   $.getJSON( '/igMediaCounts')
     .done(function( data ) {
       var yCounts = data.users.map(function(item){
-        return item.counts.media;
+          return item.counts.media;
       });
-      
+
       yCounts.unshift('Media Count');
 
       var chart = c3.generate({
         bindto: '#chart',
         data: {
           columns: [
-            yCounts 
+            yCounts
           ]
         }
       });
